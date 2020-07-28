b2a5d0b38db00d7c537003953be384e010de5d9a
Arthur <arthurd800@wp.pl>
2020-07-28 12:19:13 +0200

    finished section 9 started section 10

---
+++ src/s9/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr />
+                <p>{{ selectedComponent }}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>default content</p>
+                    </component>
+                </keep-alive>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Quote from './components/Quote';
+import Author from './components/Author';
+import New from './components/New';
+export default {
+    data: function() {
+        return {
+            quoteTitle: 'The Quote',
+            selectedComponent: 'appQuote',
+        };
+    },
+    components: {
+        appQuote: Quote,
+        appAuthor: Author,
+        appNew: New,
+    },
+};
+</script>
+<style scoped>
+h2 {
+    color: red;
+}
+</style>
---
+++ src/s9/components/Quote.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="title">
+            <slot name="title"></slot>
+            <span style="color: #ccc;">
+                <slot name="subtitle">The Subtitle</slot>
+            </span>
+        </div>
+        <hr />
+        <div class="content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px #000;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+}
+.title {
+    font-style: italic;
+}
+.content {
+    color: #787878;
+    line-height: 2;
+    text-indent: 24px;
+    position: relative;
+}
+</style>
