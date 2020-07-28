9e1fe7ed6fb371157813664870dfbe1b36313a1d
Arthur <arthurd800@wp.pl>
2020-07-28 15:19:05 +0200

    finished section 10, started section 11

---
+++ src/s10/App.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container">
+        <appHeader
+            :quoteCount="quotes.length"
+            :maxQuotes="maxQuotes"
+        ></appHeader>
+        <newQuote @quoteAdded="newQuote"></newQuote>
+        <quoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"></quoteGrid>
+        <div class="row">
+            <div class="col col-sm-12 text-center my-4">
+                <div class="alert alert-info">
+                    Info: Click on a Quote to Delete it
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
+
+export default {
+    data: function() {
+        return {
+            maxQuotes: 10,
+            quotes: [
+                'I need to move',
+                'I need to transform',
+                'People are stupid and so am I',
+            ],
+        };
+    },
+    methods: {
+        newQuote(quote) {
+            if (this.quotes.length >= this.maxQuotes) {
+                return alert('Please delete quotes first');
+            }
+            this.quotes.push(quote);
+        },
+        deleteQuote(index) {
+            this.quotes.splice(index, 1);
+        },
+    },
+    components: {
+        quoteGrid: QuoteGrid,
+        newQuote: NewQuote,
+        appHeader: Header,
+    },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Arizonia&display=swap');
+</style>
---
+++ src/s10/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+        <div class="col col-sm-12">
+            <h3>Quotes Added</h3>
+            <div class="progress">
+                <div
+                    class="progress-bar"
+                    role="progressbar"
+                    aria-valuenow="60"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    :style="calculatedWidth"
+                >
+                    {{ quoteCount }} / {{ maxQuotes }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['quoteCount', 'maxQuotes'],
+    computed: {
+        calculatedWidth() {
+            return {
+                width: (this.quoteCount / this.maxQuotes) * 100 + '%',
+            };
+        },
+    },
+};
+</script>
