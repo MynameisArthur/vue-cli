4e93967f243a61d6849335de2554cd7fb575df2e
Arthur <arthurd800@wp.pl>
2020-07-30 18:09:07 +0200

    fisnished section 12, started section 13

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
-
+            <div
+                class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+            >
+                <h1>Filters & Mixins</h1>
+                <p>{{ text | toUppercase }}</p>
+                <p>{{ text | toLowercase | toAlertCase }}</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-    }
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+export default {
+    data() {
+        return {
+            text: 'Hello there!',
+        };
+    },
+    filters: {
+        toUppercase(value) {
+            return value.toUpperCase();
+        },
+    },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
