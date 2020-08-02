fcce5b0e2c8bf7dd8890e35e34914bc58d4cc63e
Arthur <arthurd800@wp.pl>
2020-08-02 13:51:09 +0200

    finished section 15, started section 16

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Http</h1>
+            <div
+                class="col col-xs-12 col-sm-8 col-sm-offset2 col-md-6 col-md-offset-3"
+            >
+                <h1>Routing</h1>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-    }
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+export default {
+    data() {
+        return {};
+    },
+};
 </script>
 
-<style>
-</style>
+<style></style>
